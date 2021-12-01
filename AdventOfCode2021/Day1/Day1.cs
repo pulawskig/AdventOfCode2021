2a8608a6eabad5a95d8c7c4756c216cfa9d85fe7
Grzegorz Puławski <31245410+pulawskig@users.noreply.github.com>
2021-12-01 21:50:17 +0100

    Day 1

---
+++ AdventOfCode2021/Day1/Day1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public class Day1
+    {
+        public static async Task SolvePart1()
+        {
+            var inputs = (await File.ReadAllLinesAsync(@"Day1\Day1Input.txt"))
+                .Where(str => !string.IsNullOrEmpty(str))
+                .Select(int.Parse)
+                .ToArray();
+
+            var count = inputs.Skip(1)
+                .Zip(inputs.SkipLast(1), (a, b) => a - b)
+                .Count(x => x > 0);
+
+            Console.WriteLine($"Day 1 part 1: {count}");
+        }
+
+        public static async Task SolvePart2()
+        {
+            var inputs = (await File.ReadAllLinesAsync(@"Day1\Day1Input.txt"))
+                .Where(str => !string.IsNullOrEmpty(str))
+                .Select(int.Parse)
+                .ToArray();
+
+            var windows = Enumerable.Repeat(0, inputs.Length).ToArray();
+
+            for (var i = 0; i < inputs.Length; i++)
+            {
+                var val = inputs[i];
+
+                if (i > 1)
+                    windows[i - 2] += val;
+                if (i > 0)
+                    windows[i - 1] += val;
+                windows[i] += val;
+            }
+
+            var count = windows.Skip(1)
+                .Zip(windows.SkipLast(1), (a, b) => a - b)
+                .Count(x => x > 0);
+
+            Console.WriteLine($"Day 1 part 2: {count}");
+        }
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode2021;
+
+await Day1.SolvePart1();
+await Day1.SolvePart2();
